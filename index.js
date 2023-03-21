d5b0cfb207dc4c1a5995064e31a526597f6b711a
JakubSapko <jakub.sapko1@gmail.com>
2023-03-21 09:40:33 +0100

    add: simple win condition

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
-const statusDisplay = document.querySelector('.game--status');
-const optionForm = document.querySelector('.option--form');
-const submitButton = document.getElementById('formSubmit')
+const statusDisplay = document.querySelector(".game--status");
+const optionForm = document.querySelector(".option--form");
+const submitButton = document.getElementById("formSubmit");
+const cells = document.getElementsByClassName("cell");
 
-// const playerIcon = 
+let currentPlayer = "X";
+// H - Human, C - Computer
+let opponent = "H";
+let gameOver = false;
 
-optionForm.addEventListener(
-    "submit", (e) => {
-        const optionData = new FormData(optionForm);
-        for (const entry of optionData) {
-            console.log(entry);
-        }
-        submitButton.disabled = true;
-        e.preventDefault();
+optionForm.addEventListener("submit", (e) => {
+    const optionData = new FormData(optionForm);
+    for (const entry of optionData) {
+        console.log(entry);
     }
-)
-let gameLoop = true;
+    submitButton.disabled = true;
+    startGame();
+    e.preventDefault();
+});
+
+// main game "loop"
+const startGame = () => {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("click", () => {
+            console.log('elo');
+            if (!gameOver && cells[i].innerHTML === "") {
+                cells[i].innerHTML = currentPlayer;
+                checkWinner();
+                if (opponent === "computer" && !gameOver) {
+                    makeComputerMove();
+                } else {
+                    currentPlayer = currentPlayer === "X" ? "O" : "X";
+                }
+            }
+        });
+    }
+};
+
+const winCondition = (a, b, c) => cells[a].innerHTML !== '' && cells[a].innerHTML === cells[b].innerHTML && cells[b].innerHTML === cells[c].innerHTML;
+
+const checkWinner = () => {
+    const winningPositions = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+    for (let i = 0; i < winningPositions.length; i++){
+        const [a, b, c] = winningPositions[i];
+        if (winCondition(a, b, c)){
+            gameOver = true;
+            statusDisplay.innerHTML = `${currentPlayer} wins!`;
+        }
+    }
+}
